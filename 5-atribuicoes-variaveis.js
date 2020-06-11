5ac64ff63d6b35dcfb5244a7b095e2182ab6958c
tnfranco <tiago.nf94@gmail.com>
2020-06-11 18:24:46 -0300

    new file

---
+++ 5-atribuicoes-variaveis.js
@@ -0,0 +1,12 @@
+console.log("trabalhando com variaveis")
+
+const idade = 20;
+const nome = "Tiago";
+const sobrenome = "Franco";
+
+
+console.log(nome, sobrenome, idade);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
