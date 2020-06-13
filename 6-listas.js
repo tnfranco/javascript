887a6310f2a1a16d7ec167f892d32ff222111398
tnfranco <tiago.nf94@gmail.com>
2020-06-12 23:04:21 -0300

    update new file

---
+++ 6-listas.js
@@ -0,0 +1,18 @@
+console.log("trabalhando com listas")
+
+// const salvador =`Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+// const Curitiba = `Curitiba`;
+// const beloHorizonte = `Belo Horizonte`;
+
+const listaDeDestinos = new Array(
+    `Salvador`
+    `São Paulo`
+    `Rio de Janeiro`
+    `Curitiba`
+    `Belo Horizonte`
+);
+
+console.log("Listas de destinos: ");
+console.log(listaDeDestinos);
