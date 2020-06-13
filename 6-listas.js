b602da8929cbc5f709322fcb5cbbbb6a63904ad4
tnfranco <tiago.nf94@gmail.com>
2020-06-12 23:53:05 -0300

    update file

--- 6-listas.js
+++ 6-listas.js
@@ -6,10 +6,17 @@ console.log("trabalhando com listas")
 // const Curitiba = `Curitiba`;
 // const beloHorizonte = `Belo Horizonte`;
 
-const listaDeDestinos = new Array(
-    `Salvador`
+const listaDeDestinos = new Array( // cria uma lista com varias variaveis
+    `Salvador`,
+    `São Paulo`,
+    `Buenos Aires`
 );
 
-listaDeDestinos.push('Manaus')
+listaDeDestinos.push('Manaus') // adiciona um novo item na lista
 console.log("Listas de destinos: ");
 console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1,1); // remove o item da lista, de acordo com sua posição
+console.log(listaDeDestinos);
+
+console.log(listaDeDestinos[1]); // imprimi um item na lista de acordo com a posição que voce por
