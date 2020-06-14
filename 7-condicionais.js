fc454f6ccf038774921445549dafb1f58eaa1e1a
tnfranco <tiago.nf94@gmail.com>
2020-06-14 00:12:06 -0300

    update file

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,10 +6,12 @@ const listaDeDestinos = new Array(
     `Buenos Aires`
 );
 
-const idadeComprador = 21;
+const idadeComprador = 15;
+const estaAcompanhado = true;
+
 listaDeDestinos.push('Manaus') // adiciona um novo item na lista
-console.log("Listas de destinos: ");
-console.log(listaDeDestinos);
+    console.log("Listas de destinos: ");
+    console.log(listaDeDestinos);
 
 if(idadeComprador >= 18) {
     console.log("Comprador maior de idade");
@@ -17,8 +19,15 @@ if(idadeComprador >= 18) {
 }
 
 else{
+ if(estaAcompanhado == true) {
+    console.log("Comprador menor de idade, mas está acompnhado pelos pais");
+    listaDeDestinos.splice(1,1); 
+
+    }
+else{ 
     console.log("Comprador é menor de idade e não pode realizar a compra");
 }
+}
 
-console.log(listaDeDestinos);
+    console.log(listaDeDestinos);
 
