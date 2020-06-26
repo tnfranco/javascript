6ff7937c66731df4c33641a1a755a1889878c4c1
tnfranco <tiago.nf94@gmail.com>
2020-06-25 22:16:13 -0300

    new file

---
+++ index.js
@@ -0,0 +1,13 @@
+const cliente1Nome = "Tiago";
+const cliente1CPF = 43209491195;
+const cliente1Agencia = 0220;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Adriano";
+const cliente2CPF = 54289491193;
+const cliente2Agencia = 0220;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
+
+
