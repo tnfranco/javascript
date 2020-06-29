30f2d1e74ceb9fb15877c3e1004bcacb4417ad2e
tnfranco <tiago.nf94@gmail.com>
2020-06-28 23:13:24 -0300

    update file

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const cliente1Nome = "Tiago";
-const cliente1CPF = 43209491195;
-const cliente1Agencia = 0220;
-const cliente1Saldo = 0;
+class Cliente { 
+    
+    constructor(nome, cpf, agencia, saldo) {
+      this.nome = nome;
+      this.cpf = cpf;
+      this.agencia = agencia;
+      this.saldo = saldo;
+    }
+}
+  
+  cliente1 = new Cliente("Tiago", 4234234242423, 0220, 9999999.99);
+  cliente2 = new Cliente("Adriano", 94397573939, 0220, 9439349.94);
+  cliente3 = new Cliente("Aline", 7499393943943, 0220, 9439439.94);
 
-const cliente2Nome = "Adriano";
-const cliente2CPF = 54289491193;
-const cliente2Agencia = 0220;
-const cliente2Saldo = 0;
-
-console.log(cliente1Nome);
+    console.log(cliente1, cliente2, cliente3);
 
 
+  
